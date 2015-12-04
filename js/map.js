845fe9310780776941930109ca753cdbd9e89907
Alex Mandel <aimandel@ucdavis.edu>
2015-12-03 16:42:47 -0800

    Custom Icon

--- js/map.js
+++ js/map.js
@@ -19,7 +19,7 @@ function initMap() {
     //    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     //}).addTo(map);
 
-    var LeafIcon = L.Icon.extend({
+   /* var LeafIcon = L.Icon.extend({
         options: {
             shadowUrl: 'http://cdn.leafletjs.com/leaflet/v0.7.7/images/marker-shadow.png',
             iconSize:     [38, 95],
@@ -28,11 +28,22 @@ function initMap() {
             shadowAnchor: [4, 62],
             popupAnchor:  [-3, -76]
         }
-    });
+    });*/
 
-    var greenHouse = new LeafIcon({iconUrl: 'images/favicon-noback.png'});
+    //var greenHouse = new LeafIcon({iconUrl: 'images/favicon-noback.png'});
+    var greenIcon = L.icon({
+    iconUrl: 'images/leaf-green.png',
+    shadowUrl: 'images/leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
     
-    L.marker([38.56, -121.5,{icon: greenHouse}]).addTo(map)
+    L.marker([38.56, -121.5],{icon: greenIcon}).addTo(map)
         .bindPopup('Your Urban Farm,<br> Grow!')
         .openPopup();
 }
